b900342c19c664cd70d818ec4f2c2a77994b2ed0
Eng-Nafad <nafadayman@yahoo.com>
2025-10-16 22:09:34 +0300

    Community added without dark mode

---
+++ src/Pages/Community/Components/Community_Header.jsx
@@ -0,0 +1,23 @@
+
+const Community_Header = () => {
+  return (
+		<div className="mb-8">
+			<div className="flex justify-evenly pt-10 items-center">
+				<div className="flex gap-3 flex-col">
+					<h1 className="font-bold text-2xl">Community</h1>
+					<p className="text-[#6b7685]">Join discussions, share your thoughts, and connect with fellow developers</p>
+				</div>
+				<button className="bg-mainGradient flex justify-center items-center gap-3 px-2 py-1.5 text-white font-medium	 rounded-lg">
+					<h1>+</h1>
+					<h1>Create Post</h1>
+				</button>
+			</div>
+			
+		</div>
+  )
+}
+
+export default Community_Header
+
+
+
---
+++ src/Pages/Community/Components/Tabs_section.jsx
@@ -0,0 +1,73 @@
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
+import { FiMessageSquare } from "react-icons/fi";
+import { FaRegChartBar } from "react-icons/fa";
+import { IoTrendingUp } from "react-icons/io5";
+import Polls from "./Tabs_info/Polls";
+import Trending from "./Tabs_info/Trending";
+import Discussions from "./Tabs_info/Discussions";
+
+
+
+
+
+
+const Tabs_section = () => {
+	const data = [
+    {
+      label: "Discussions",
+			icon : <FiMessageSquare/>,
+      value: "discussions",
+      desc: <Discussions/>,
+    },
+    {
+      label: "Polls",
+			icon : <FaRegChartBar/>,
+      value: "polls",
+      desc: <Polls/>,
+    },
+    {
+      label: "Trending",
+			icon : <IoTrendingUp/>,
+      value: "trending",
+      desc: <Trending/>,
+    },
+];
+  return (
+		<div className="!grid !grid-cols-5">
+			<Tabs id="custom-animation" value="discussions" className="col-start-2 col-end-5">
+				<TabsHeader>
+					{data.map(({ label, value,icon }) => (
+						<Tab key={value} value={value}>
+							<div className="flex flex-row justify-center items-center gap-3 font-semibold text-sm">
+								{icon}
+								{label}
+							</div>
+						</Tab>
+					))}
+				</TabsHeader>
+				<TabsBody
+					animate={{
+						initial: { y: 250 },
+						mount: { y: 0 },
+						unmount: { y: 250 },
+					}}
+				>
+					{data.map(({ value, desc }) => (
+						<TabPanel key={value} value={value}>
+							{desc}
+						</TabPanel>
+					))}
+				</TabsBody>
+			</Tabs>
+		</div>
+
+  );
+}
+
+export default Tabs_section
