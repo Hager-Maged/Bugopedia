f7331bd6aa9510519a4aa3d4ece8a527c1c802d5
Mariam Yasser <yassermariam444@gmail.com>
2025-10-17 15:40:38 +0300

    Fix import path for Signin component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Profile from "./Pages/Profile/Profile";
 import NavBar from "./Components/NavBar/Nav";
 import Signin from "./Pages/Signin/Signin";
 import FAQ from "./Pages/FAQ/FAQ";
-import Signin from "./Pages/Signin/Signin";
+import Signin from "./Pages/SignIn/SignIn";
 
 function App() {
   if (
