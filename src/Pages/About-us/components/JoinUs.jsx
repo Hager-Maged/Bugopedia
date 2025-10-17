c552d3d4628373fdf19191d4c1ee4c02c9a1ec07
KamelMohamed <kamel.mohamed.zidan.22@gmail.com>
2025-10-17 03:00:48 +0300

    added links in about=>join us section

--- src/Pages/About-us/components/JoinUs.jsx
+++ src/Pages/About-us/components/JoinUs.jsx
@@ -1,17 +1,25 @@
+import { Link } from "react-router-dom";
+
 import ButtonGradiant from "../../../Components/Buttons/ButtonGradiant";
 import ButtonLight from "../../../Components/Buttons/ButtonLight";
 
 const JoinUs = () => {
   return (
     <div className="pb-10 flex flex-col items-center justify-center gap-5 py-10 rounded-xl px-5 border border-orangeColor dark:bg-mutedYellow ">
-      <h3 className="text-blackText dark:text-whiteText text-lg font-semibold">Join Our Community</h3>
+      <h3 className="text-blackText dark:text-whiteText text-lg font-semibold">
+        Join Our Community
+      </h3>
       <p className="text-grayText text-l font-semibold">
         Be part of a global community of developers helping each other build
         better software. Start debugging together today.
       </p>
       <div className="flex gap-3">
-        <ButtonGradiant text="Get Started" size="l"/>
-        <ButtonLight text="Learn More" size="l" />
+        <Link to="/signin">
+          <ButtonGradiant text="Get Started" size="l" />
+        </Link>
+        <Link to="/faq">
+          <ButtonLight text="Learn More" size="l" />
+        </Link>
       </div>
     </div>
   );
