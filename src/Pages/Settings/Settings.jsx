715120a25e66b4aba1d098214ffccc1b262a9ec6
KamelMohamed <kamel.mohamed.zidan.22@gmail.com>
2025-10-18 15:58:01 +0300

    Settings set up

--- src/Pages/Settings/Settings.jsx
+++ src/Pages/Settings/Settings.jsx
@@ -1,7 +1,32 @@
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
+import {
+  Square3Stack3DIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+} from "@heroicons/react/24/solid";
+
 const Settings = () => {
+  const data = [
+    {
+      label: "Kamel",
+      value: "kamel",
+      desc: "Hi all",
+    },
+  ];
+
   return (
-    <div className="bg-whiteText dark:bg-blackText">
-        {/* TO BE DONE */}
+    <div className="bg-whiteText dark:bg-darkModeBg lg:px-32 md:px-20 px-5">
+      <div className="">
+        <h1>Settings</h1>
+        <p>Manage your account settings and preferences</p>
+        asdada
+      </div>
     </div>
   );
 };
